0dffe86426b9d97eb8af003b699b4b32515777a2
drussell1974 <dave@jazzthecat.co.uk>
2019-04-18 14:29:01 +0100

    update recursion algorithms

--- recursion/recursion_simple.py
+++ recursion/recursion_simple.py
@@ -1,11 +1,6 @@
-import sys
-print(sys.getrecursionlimit())
-#sys.setrecursionlimit(6000)
-
 def recursion_made_simple(stop_at, current):
     if current > stop_at:
         return 1
     return recursion_made_simple(stop_at, current+1)
     
-    
-print(recursion_made_simple(2999, 1))
+print(recursion_made_simple(2987, 1))
