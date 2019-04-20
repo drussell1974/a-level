ea0fefed6a480f868e7b48a5a7f7392f6a1377c9
drussell1974 <dave@jazzthecat.co.uk>
2019-04-20 07:06:08 +0100

    show dictionary vs custom hash table

--- hash_table/hash_table.py
+++ hash_table/hash_table.py
@@ -4,6 +4,7 @@ class hash_table:
         """ Create a fixed size list based on the maxium number keys """
         self.max = max
         self.lst = []
+        
         for i in range(0, self.max):
             self.lst.append(None)
     
@@ -12,42 +13,88 @@ class hash_table:
         num = 0
         for i in range(len(key)-1):
             num = num + ord(key[i]) * i
-        print(num % max)
         return num % max
     
     
     def __getitem__(self, key):
         """ get the key from the list """
-        return self.lst[self._hash(key, self.max)]
+        
+        h = self._hash(key, self.max)
+        
+        return self.lst[h]
     
     
     def __setitem__(self, key, value):
         """ add the key to the list """
-        # add to the the list
-        self.lst[self._hash(key, self.max)] = value
+        
+        h = self._hash(key, self.max)
+        
+        self.lst[h] = value
         
     
     def __delitem__(self, key):
         """ void key """
-        self.lst[self._hash(key, self.max)] = None
+        
+        h = self._hash(key, self.max)
+        
+        self.lst[h] = None
     
     
     def __str__(self):
-        """ returns a string representation of the object using 'str(list)' (or 'print(list)') """
+        """
+        returns a string representation of the
+        object using 'str(list)' (or 'print(list)')
+        """
+        
         return str(self.lst)
         
-        
+# dictionary
 
-products = hash_table(256)
-products["AA123"] = "burgar"  # add the first item
-products["AZ123"] = "ice cream"  # add a second item
-products["ZA2"] = "fries" # add a collision
-products["AA123"] = "burger" # update the first item
-products["ZZ"] = "veggie" # add a last item
-print(products["AZ"]) # show the first item in the collision
-print(products["ZA"]) # show the second item in the collision
-print(products["AB"]) # show the updated value
+products = {
+    "AA123":"burgar",
+    "AZ456":"ice cream"
+    }
+
 print(products)
+
+# add
+
+products["B789"] = "fries"
+products["ZZ"] = "veggie"
+print(products)
+
+# update
+products["AA123"] = "burger"
+print(products)
+
+# delete
+del products["B789"]
+print(products)
+
+# custom hash table
+print("MY CUSTOM HASH TABLE")
+
+my_hash_table = hash_table(256)
+print(my_hash_table)
+
+# add
+my_hash_table["AA123"] = "burgar"
+my_hash_table["AZ456"] = "ice cream"
+my_hash_table["B789"] = "fries"
+my_hash_table["ZZ"] = "veggie"
+print(my_hash_table)
+
+# update
+print(my_hash_table["AA123"])
+my_hash_table["AA123"] = "burger"
+print(my_hash_table["AA123"])
+
+# delete
+del my_hash_table["B789"]
+print(my_hash_table)
+
+
+        
 
 
         
