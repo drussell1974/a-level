d1ca9cb18d09fa023d7ad143350756dcc6dabf8e
Dave Russell <drussell1974@users.noreply.github.com>
2019-04-30 07:40:25 +0100

    Update classes.py

--- oop/classes.py
+++ oop/classes.py
@@ -1,15 +1,18 @@
 class Base:
     def __init__(self, id):
-        self.id = id
+        self.id = int(id)
         self.errors = []
        
     def has_errors(self):
         return len(self.errors) > 0
     
+    def get_errors(self):
+        return self.errors
+    
 
 class Product:
     def __init__(self, id = 0, name = "", price = 0.0, quanity_in_stock = 0):
-        self.id = int(id)
+        self.super().__init__(id)
         self.name = str(name)
         print(price)
         self.price = float(price)
@@ -18,5 +21,6 @@ class Product:
         
 class ProductType:
     def __init__(self, id, name):
+        self.super().__init__(id)
         self.id = int(id)
         self.name = name
